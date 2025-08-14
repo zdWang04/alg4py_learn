a508fc753bad82386dd33c3ce4ce1115d279833a
zdWang04 <zdw04@outlook.com>
2025-08-14 15:58:32 +0800

    Stack的类型提示 + readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# alg4py
+
+## Stack Implementation
+
+`Stack.py` contains a basic implementation of a stack data structure in Python. 
+
--- basic_data_structure/stack.py
+++ basic_data_structure/stack.py
@@ -1,10 +1,35 @@
-class Stack:
-  def __init__(self):
-	self.stack = []
-  def isEmpty(self):
-	return self.stack == []
-  def push(self, something):
-	return self.stack.push(something)
-  def pop(self):
-	return self.stack.pop()
+from typing import Any
 
+
+class Stack:
+    def __init__(self):
+        self.stack: list[Any] = []
+
+    def isEmpty(self) -> bool:
+        return self.stack == []
+
+    def push(self, something: Any) -> None:
+        return self.stack.append(something)
+
+    def pop(self) -> Any:
+        return self.stack.pop()
+
+    def size(self) -> int:
+        return len(self.stack)
+
+    def peek(self) -> Any:
+        return self.stack[self.size() - 1]
+
+
+# def test1():
+#     s = Stack()
+#     assert s.isEmpty() is True
+#     s.push(1)
+#     assert s.isEmpty() is False
+#     assert s.pop() == 1
+#     assert s.isEmpty() is True
+
+
+# if __name__ == "__main__":
+#     test1()
+#     print("Stack test passed.")
