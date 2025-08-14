a179f3a750fbb14d11c9e5866c882b983eb22586
zdWang04 <zdw04@outlook.com>
2025-08-14 17:16:36 +0800

    完成了dec2any函数，支持10进制向1~16进制转换

--- basic_data_structure/stack/base_converter.py
+++ basic_data_structure/stack/base_converter.py
@@ -18,7 +18,25 @@ def dec2bi(decNum: int) -> str:
     return bi_str
 
 
-# def test():
+def dec2any(decNum: int, base: int) -> str:
+    num_str = "0123456789ABCDEF"
+    assert (base <= 16) and (base > 0), "最多支持16进制的转化"
+    if decNum == 0:
+        return "0"
+
+    rem_stack = Stack()
+    while decNum > 0:
+        rem = decNum % base
+        rem_stack.push(rem)
+        decNum //= base
+    target_str = ""
+    while not rem_stack.isEmpty():
+        num = num_str[rem_stack.pop()]
+        target_str += num
+    return target_str
+
+
+# def test4dec2bi():
 #     assert dec2bi(0) == "0"
 #     assert dec2bi(1) == "1"
 #     assert dec2bi(2) == "10"
@@ -27,6 +45,18 @@ def dec2bi(decNum: int) -> str:
 #     assert dec2bi(255) == "11111111"
 
 
+# def test4dec2any():
+#     assert dec2any(0, 2) == "0"
+#     assert dec2any(10, 2) == "1010"
+#     assert dec2any(255, 2) == "11111111"
+#     assert dec2any(255, 8) == "377"
+#     assert dec2any(255, 10) == "255"
+#     assert dec2any(255, 16) == "FF"
+#     assert dec2any(1234, 16) == "4D2"
+#     assert dec2any(1234, 8) == "2322"
+
+
 # if __name__ == "__main__":
-#     test()
+#     test4dec2bi()
+#     test4dec2any()
 #     print("All test done!")
