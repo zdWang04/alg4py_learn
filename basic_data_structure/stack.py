24facddca8610b852d22c61aff66cbfe6caee25a
zdWang04 <zdw04@outlook.com>
2025-08-14 14:05:19 +0800

    补完了stack的其他方法

--- basic_data_structure/stack.py
+++ basic_data_structure/stack.py
@@ -3,4 +3,8 @@ class Stack:
 	self.stack = []
   def isEmpty(self):
 	return self.stack == []
+  def push(self, something):
+	return self.stack.push(something)
+  def pop(self):
+	return self.stack.pop()
 
